3c9d8298fb1cc9c637075aca742a56e7f9f43fd8
Helen Shin <helenhyshin@gmail.com>
2023-01-10 13:27:14 -0800

    add functionality to search movies using api

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
+
+// because we are waiting for fetchData to get data and access response, use async and await
+const onInput = debounce(async (event) => {
+  const movies = await fetchData(event.target.value);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <img src ="${movie.Poster}" />
+    <h1>${movie.Title}</h1>
+    `;
+    document.querySelector("#target").appendChild(div);
   }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 500);
-};
-// on keyup, start keyupTimer
-// if keyupTimer is greater than half a second, send API request
-// else clearTimeout
+}, 500);
+
 input.addEventListener("input", onInput);
